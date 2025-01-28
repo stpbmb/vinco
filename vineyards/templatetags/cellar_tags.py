61eaec82a82a5479e1d7eaccb0a998d5d6f0978a
Stjepan Bumba <stpbmb@gmail.com>
2025-01-28 15:55:50 +0100

    fixed Tank History view errors

--- vineyards/templatetags/cellar_tags.py
+++ vineyards/templatetags/cellar_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from vineyards.models import Vineyard
 
 register = template.Library()
 
@@ -7,5 +8,14 @@ def sub(value, arg):
     """Subtract the arg from the value."""
     try:
         return float(value) - float(arg)
+    except (ValueError, TypeError):
+        return value
+
+@register.filter
+def get_grape_variety_display(value):
+    """Convert grape variety choice to display name."""
+    try:
+        choices_dict = dict(Vineyard.GRAPE_VARIETY_CHOICES)
+        return choices_dict.get(value, value)
     except (ValueError, TypeError):
         return value
